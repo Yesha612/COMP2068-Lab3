be8bef59fdb9bd4d7feacab95a7a7dad3a1b7e7e
Yesha612 <yesha06121999@gmail.com>
2019-05-30 23:53:59 -0400

    Added variables

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,42 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/yesha', (req, res, next) => {
-  res.render('yesha', {});
+  res.render('yesha', 
+  {
+    age: 16,
+    birthPlace: 'Anand',
+    interests: 'Reading novels',
+   });
 });
 
 router.get('/deep', (req, res, next) => {
-  res.render('deep', {});
+  res.render('deep', 
+  {
+    age: 15,
+    birthPlace: 'Amod',
+    interests: 'Sports',
+    relation: 'brother'
+  });
 });
 
 router.get('/ronak', (req, res, next) => {
-  res.render('ronak', {});
+  res.render('ronak', 
+  {
+    age: 45,
+    birthPlace: 'Ranoli',
+    interests: 'Accounts',
+    relation: 'father'
+  });
 });
 
 router.get('/priti', (req, res, next) => {
-  res.render('priti', {});
+  res.render('priti', 
+  {
+    age: 42,
+    birthPlace: 'Vadod',
+    interests: 'Religious activities',
+    relation: 'mother'
+  });
 });
 
 module.exports = router;
