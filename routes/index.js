19cc0b4e994757067280ace0629bcf5eed2316a3
Yesha612 <yesha06121999@gmail.com>
2019-05-31 00:54:45 -0400

    Made it easier to add members

--- routes/index.js
+++ routes/index.js
@@ -6,43 +6,54 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Family' });
 });
 
-router.get('/yesha', (req, res, next) => {
-  res.render('yesha', 
-  {
-    age: 16,
+const members = [
+{
+    path: 'yesha',
+    name: 'Yesha Patel',
+    age: 19,
     birthPlace: 'Anand',
-    interests: 'Reading novels',
-   });
-});
-
-router.get('/deep', (req, res, next) => {
-  res.render('deep', 
-  {
+    interests: 'Reading novels'
+}, 
+{
+    path: 'deep',
+    name: 'Deep Patel',
     age: 15,
     birthPlace: 'Amod',
     interests: 'Sports',
     relation: 'brother'
-  });
-});
-
-router.get('/ronak', (req, res, next) => {
-  res.render('ronak', 
-  {
+}, 
+{
+    path: 'ronak',
+    name: 'Ronak Patel',
     age: 45,
     birthPlace: 'Ranoli',
     interests: 'Accounts',
     relation: 'father'
-  });
-});
-
-router.get('/priti', (req, res, next) => {
-  res.render('priti', 
-  {
+},  
+{
+    path: 'priti',
+    name: 'Priti Patel',
     age: 42,
     birthPlace: 'Vadod',
     interests: 'Religious activities',
     relation: 'mother'
-  });
+}
+];
+
+router.get('/:name', (req, res, next) => {
+  const name = req.params.name;
+
+  // Search the members array and find where the name variable is equal to the path in family
+  const family = members.find(mem => mem.path === name);
+
+  //if no member - generates error and passes to next function
+  if (!family) {
+    return next(new Error('Member does not exist'))
+  }
+
+  // Pass our found member to the family view
+  res.render('family', family);
 });
+
 
 module.exports = router;
